80c47297d1bbc0882f14d39678c603de5a11bdf7
Gabriel Hoppe <hoppecsgonet@gmail.com>
2022-04-11 17:12:33 -0300

    mount minimum path in matrix

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@ struct coords {
     int n;
 };
 
+
+// start -> 0,0
+// end -> 6,6
 int matrix[7][7] = {
     { 1, 1, 1, 1, 1, 0, 1 },
     { 1, 0, 0, 0, 1, 0, 1 },
@@ -55,6 +58,19 @@ int getLeft(coords c) {
     return matrix[c.i][c.j - 1];
 }
 
+void showNumericMatrix() {
+
+    for (int i = 0; i < 7; i++) {
+
+        for (int j = 0; j < 7; j++) {
+            
+            std::cout << matrix[i][j] << " ";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
 int main() {
 
     std::queue<int> paths[1024];
@@ -72,8 +88,6 @@ int main() {
         int n = firstItem.n - 1;
 
         matrix[firstItem.i][firstItem.j] = n;
-
-        std::cout << firstItem.i << "," << firstItem.j << ":" << n << std::endl;
 
         myQueue.pop();
 
@@ -108,15 +122,62 @@ int main() {
     
     std::cout << std::endl << std::endl;
 
-    for (int i = 0; i < 7; i++) {
+    showNumericMatrix();
 
-        for (int j = 0; j < 7; j++) {
-            
-            std::cout << matrix[i][j] << " ";
+    std::cout << std::endl << std::endl;
+
+    // start -> 0,0
+    // end -> 6,6
+
+    std::queue<coords> drawQueue;
+    coords endPos = { 6, 6, 0 };
+
+    drawQueue.push(endPos);    
+
+    while (!drawQueue.empty()) {
+
+        coords firstItem = drawQueue.front();
+
+        int firstItemValue = matrix[firstItem.i][firstItem.j];
+
+        matrix[firstItem.i][firstItem.j] = 32;
+
+        drawQueue.pop();
+
+        if (firstItem.i -1 >= 0 && getTop(firstItem) == firstItemValue + 1) {
+
+            // top -> (i-1, j)
+            coords topCoords = { firstItem.i -1 , firstItem.j, 0 };
+            drawQueue.push(topCoords);
         }
 
-        std::cout << std::endl;
+        if (firstItem.j + 1 <= 7 && getRight(firstItem) == firstItemValue + 1) {
+
+            // right -> (i, j+1)
+            coords rightCoords = { firstItem.i, firstItem.j + 1, 0 };
+            drawQueue.push(rightCoords);
+        }
+
+        if (firstItem.i + 1 <= 7 && getBottom(firstItem) == firstItemValue + 1) {
+
+            // bottom -> (i+1, j)
+            coords bottomCoords = { firstItem.i + 1, firstItem.j, 0 };
+            drawQueue.push(bottomCoords);
+        }
+
+        if (firstItem.j - 1 >= 0 && getLeft(firstItem) == firstItemValue + 1) {
+
+            // left -> (i, j -1)
+            coords leftCoords = { firstItem.i, firstItem.j - 1, 0 };
+            drawQueue.push(leftCoords);
+        }
     }
+
+    std::cout << std::endl << std::endl;
+
+    showNumericMatrix();
+
+    std::cout << std::endl << std::endl;
 
     system("pause");
     return 0;
